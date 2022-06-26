cbac3d2b960534b1f28ee00fcdaf91ae9ea6a8e8
Reema <Reemaalshohof88@gmail.com>
2022-06-26 12:13:34 +0200

    copy all files from private repo to public repo

---
+++ server/src/controllers/user/getAuthUser.js
@@ -0,0 +1,31 @@
+import User from "../../models/User.js";
+import jwt from "jsonwebtoken";
+import { logError } from "../../util/logging.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const authLogin = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const findUser = await User.findOne({ email }).select("-password");
+    if (!findUser) {
+      return res.status(404).json({ success: false, msg: "User is not Found" });
+    }
+    const accessToken = jwt.sign(
+      { email: findUser.email, id: findUser._id },
+      process.env.JWT_ACCESS_TOKEN,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    return res.status(200).json({ success: true, user: findUser, accessToken });
+  } catch (error) {
+    logError(error);
+    return res
+      .status(500)
+      .json({ success: false, msg: "Unable to get users, try again later" });
+  }
+};
+
+export default authLogin;
