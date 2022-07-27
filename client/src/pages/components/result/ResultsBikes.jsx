7261bc389b8a93b6795b7bb4efa03504e9f8c16d
Reema <Reemaalshohof88@gmail.com>
2022-07-27 14:25:36 +0200

    change component name from  ResultPageWrapper to ResultsBikes

--- client/src/pages/components/result/ResultsBikes.jsx
+++ client/src/pages/components/result/ResultsBikes.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { BikesContext } from "../../../provider/bikes";
 import MapContainer from "../../../components/mapContainer/MapContainer";
-import BikeCardList from "./BikeCardList";
+import BikeCard from "../../../components/bikeCard/BikeCard";
 import Loading from "../../../components/loading/Loading";
 import Error from "../../../components/error/Error";
-const ResultPageWrapper = () => {
+const ResultsBikes = () => {
   const { bikes, isLoading, error } = useContext(BikesContext);
 
   if (isLoading) {
@@ -29,7 +29,26 @@ const ResultPageWrapper = () => {
   return (
     <div className="md:flex mt-10 ">
       <div className="w-full md:w-[55%] mx-auto">
-        <BikeCardList bikeResult={bikes} />
+        <div className="my-10">
+          <div className="w-full flex flex-wrap justify-center">
+            {bikes.map((bike) => {
+              return (
+                <BikeCard
+                  email={bike.email}
+                  img={bike.photos[0]}
+                  title={bike.title}
+                  brand={bike.brand.value}
+                  type={bike.type.value}
+                  price={bike.price}
+                  key={bike._id}
+                  id={bike._id}
+                  sellFaster={bike.sellFaster}
+                  className="m-2  rounded-md w-full  min-w-96 md:w-[19rem] max-w-sm 2xl:w-96  border-[1px] border-tertiary cursor-pointer shadow-lg hover:shadow-md transform transition-all duration-200 ease-in-out"
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
       {/* map container */}
       <div className="flex-1 relative">
@@ -45,4 +64,4 @@ const ResultPageWrapper = () => {
     </div>
   );
 };
-export default ResultPageWrapper;
+export default ResultsBikes;
